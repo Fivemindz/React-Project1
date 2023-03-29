bb7ececd88b486bf2caaf5e43c04008e8e1674b8
Javi <fvemndz@aol.com>
2023-03-29 03:41:36 -0500

    began search page functionality

--- starter/src/SearchPage.js
+++ starter/src/SearchPage.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import Book from './Book';
+import * as BooksAPI from "./BooksAPI";
 
 const SearchPage = () => {
 
-  
+  const [books, setBooks] = useState();
+
+  const navigate = useNavigate();
+
+  const searchBooks = async (query) => { 
+    const res = await BooksAPI.search(query.toLowerCase())
+    setBooks(res);
+  };
+
+
 
   return (
     <div className="search-books">
@@ -13,11 +24,18 @@ const SearchPage = () => {
           <input
             type="text"
             placeholder="Search by title, author, or ISBN"
+            onChange={(e) => searchBooks(e.target.value)}
           />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {books.map((book) => (
+            <li key={book.id}>
+              <Book book={book} />
+            </li>
+          ))} 
+        </ol>
       </div>
     </div>
   )
